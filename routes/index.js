b2c887bf3d738bed3090ed17081c8ad6e891d819
Philippe Richard <philippe.richard93@gmail.com>
2014-05-16 11:39:29 -0400

    Added login page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Pepito Pizzeria' });
 });
+
+router.get('/login', function(req, res) {
+  res.render('login', { title: 'Login' });
+});
+
 
 module.exports = router;
