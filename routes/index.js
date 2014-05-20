424406e1532cc7ecbe2f27e38eb6339296a548ab
Philippe Richard <philippe.richard93@gmail.com>
2014-05-20 13:18:04 -0400

    Added login support

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var crypto = require('crypto');
 var router = express.Router();
 
 /* GET home page. */
@@ -7,7 +8,37 @@ router.get('/', function(req, res) {
 });
 
 router.get('/login', function(req, res) {
-  res.render('login', { title: 'Login' });
+    res.render('login', { title: 'Login' });
+});
+
+/* POST to authentificate users */
+router.post('/authenticate', function(req, res) {
+
+    // Set our internal DB variable
+    var db = req.db;
+
+    // Get our form values. These rely on the "name" attributes
+    var userName = req.body.username;
+    var userPassword = crypto.createHash('md5').update(req.body.password).digest('hex');
+
+    // Set our collection
+    var collection = db.get('usercollection');
+    
+    //Find user that matches the username and password
+    collection.findOne({username : userName , password : userPassword},{},function(e,docs){
+        if(docs != null){
+        	//Add the user _id to a session variable
+      		req.session.uid = docs._id;
+      		
+            res.location("/users");
+            res.redirect("/users");
+    	}else{
+    		//Redirect to the login page with a "Bad credentials" error
+    		res.location("/login");
+			res.redirect("/login?e=1");
+    	}
+    });
+
 });
 
 
