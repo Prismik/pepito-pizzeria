94a4e302809bc0ccde0fd3abf09117b8c96ff2a0
EtsJC <Charestjonathan@hotmail.com>
2014-05-30 00:04:19 -0400

    started schema

--- schema/userSchema.js
+++ schema/userSchema.js
@@ -1,3 +1,21 @@
 ﻿var mongoose = require('mongoose');
 
-mongoose.connect('');
+module.export = function (db) {
+
+    mongoose.connect('localhost: 27017 / pepito - pizzeria') //change for the dev or prod
+
+    var Schema = mongoose.Schema;
+
+    var User = new Schema({
+        id: { type: Number}
+      , username: { type: String }
+      , date: { type: Date }
+      , address: { type: String }
+      , phone: { type: String }
+      , email: {type: String }
+    });
+
+    return User;
+
+}
+
