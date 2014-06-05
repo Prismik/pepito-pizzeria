6be7d2a46eaeb2afa012bc703677165a21ef396d
francoisgelinas1 <francois.gelinas5@gmail.com>
2014-06-05 19:22:25 -0400

    form validation

--- public/javascripts/validateform.js
+++ public/javascripts/validateform.js
@@ -1,40 +1,56 @@
-var validePassword;
-var valideEmail;
-
-function validatePassword(){
-    var password = document.getElementById('password');
-    var password1 = document.getElementById('password2');
-    var message = document.getElementById('message');
-    if (password.value == password1.value) {
-        message.innerHTML = "Passwords match";
-        validePassword = true;
-    }else{
-        message.innerHTML = "Passwords don't match";
-        validePassword = false;
-    }
-    validateForm();
+function validateLenghtMin(inputId, validationValue, errormessage){
+	document.getElementById(inputId).setCustomValidity(errormessage);
+	document.getElementById(inputId).addEventListener("change", function(e) {
+		if(document.getElementById(inputId).value.length >= validationValue){
+			document.getElementById(inputId).setCustomValidity("");
+		}else{
+			document.getElementById(inputId).setCustomValidity(errormessage);
+		}
+	}, false);
 }
 
-function validateEmail(){
-    var mail=document.getElementById('inputUserEmail').value;
-    var at=mail.indexOf("@");
-    var dot=mail.lastIndexOf(".");
-    if (at<1 || dot<at+2 || dot+2>=mail.length)
-    {
-        message.innerHTML = "Not a valide email address";
-        valideEmail = false;
-    }else{
-        valideEmail = true;
-        message.innerHTML = "";
-    }  
-    validateForm();  
+function validateRegex(inputId, regex, errormessage){
+	document.getElementById(inputId).setCustomValidity(errormessage);
+	document.getElementById(inputId).addEventListener("change", function(e) {
+		var expression = new RegExp(regex);
+		if(expression.test(document.getElementById(inputId).value)){
+			document.getElementById(inputId).setCustomValidity("");
+		}else{
+			document.getElementById(inputId).setCustomValidity(errormessage);
+		}
+	}, false);
 }
-function validateForm(){
-    var btnSubmit = document.getElementById("btnSubmit");
-    if (valideEmail && validePassword)
-    {
-        btnSubmit.disabled=false;
-    }else{
-        btnSubmit.disabled=true;
-    }
+
+function validatePassword(passwordId1, passwordId2, errormessage){
+	document.getElementById(passwordId2).setCustomValidity(errormessage);
+	document.getElementById(passwordId1).addEventListener("change", function(e) {
+		if(document.getElementById(passwordId1).value == document.getElementById(passwordId2).value){
+			document.getElementById(passwordId2).setCustomValidity("");
+		}else{
+			document.getElementById(passwordId2).setCustomValidity(errormessage);
+		}
+	}, false);
+	
+	document.getElementById(passwordId2).addEventListener("change", function(e) {
+		if(document.getElementById(passwordId1).value == document.getElementById(passwordId2).value){
+			document.getElementById(passwordId2).setCustomValidity("");
+		}else{
+			document.getElementById(passwordId2).setCustomValidity(errormessage);
+		}
+	}, false);
+}
+
+function validateEmail(inputId, errormessage){
+	document.getElementById(inputId).setCustomValidity(errormessage);
+	document.getElementById(inputId).addEventListener("change", function(e) {
+		var mail=document.getElementById(inputId).value;
+		var at=mail.indexOf("@");
+		var dot=mail.lastIndexOf(".");
+		if (at<1 || dot<at+2 || dot+2>=mail.length)
+		{
+    		document.getElementById(inputId).setCustomValidity(errormessage);
+		}else{
+    	    document.getElementById(inputId).setCustomValidity("");
+		}
+	}, false);
 }
