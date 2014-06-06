ead9288564b12ab8a24197bac5f50ffc03a58046
Francis <francis.beauchampsc@gmail.com>
2014-06-06 10:11:43 -0400

    Initialised Restaurants pages and controller

---
+++ routes/restaurants.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res) {
+    res.render('restaurants/list', {
+            title: 'Pepito Pizzeria - Restaurants',
+            header: 'Restaurants',
+            active: 'resto'
+        });
+});
+
+router.get('/create', function(req, res){
+    res.render('restaurants/create', {
+            title: 'Pepito Pizzeria - Users',
+            header: 'Restaurants',
+            active: 'resto'
+        });
+});
+
+router.get('/update', function(req, res){
+    res.render('restaurants/update', {
+            title: 'Pepito Pizzeria - Users',
+            header: 'Restaurants',
+            active: 'resto'
+        });
+});
+
+module.exports = router;
