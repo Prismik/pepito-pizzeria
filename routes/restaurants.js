0f7174336e75c4ad4f7b907859cb56645fd180ec
Francis <francis.beauchampsc@gmail.com>
2014-06-06 10:22:27 -0400

    Few changes, fixed misc migration 005

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -2,27 +2,31 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-    res.render('restaurants/list', {
+    var db = req.db;
+    var collection = db.get('restaurants');
+    collection.find({},{},function(e,docs){
+        res.render('restaurants/list', {
             title: 'Pepito Pizzeria - Restaurants',
             header: 'Restaurants',
             active: 'resto'
         });
+    });
 });
 
 router.get('/create', function(req, res){
     res.render('restaurants/create', {
-            title: 'Pepito Pizzeria - Users',
-            header: 'Restaurants',
-            active: 'resto'
-        });
+        title: 'Pepito Pizzeria - Users',
+        header: 'Restaurants',
+        active: 'resto'
+    });
 });
 
 router.get('/update', function(req, res){
     res.render('restaurants/update', {
-            title: 'Pepito Pizzeria - Users',
-            header: 'Restaurants',
-            active: 'resto'
-        });
+        title: 'Pepito Pizzeria - Users',
+        header: 'Restaurants',
+        active: 'resto'
+    });
 });
 
 module.exports = router;
