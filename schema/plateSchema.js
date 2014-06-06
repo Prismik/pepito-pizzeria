0c628a7e98d25b1b8e5e525e7ec31d271b977c80
EtsJC <Charestjonathan@hotmail.com>
2014-06-06 12:00:17 -0400

    more scheam

---
+++ schema/plateSchema.js
@@ -0,0 +1,28 @@
+﻿var env = process.env.NODE_ENV || 'development';
+var config = require('../config/' + env);
+var mongoose = require('mongoose');
+
+userSchema = new mongoose.Schema({
+    userid: { type: Number }
+           , username: { type: String }
+           , birthdate: { type: String }
+           , address: { type: [String] }
+           , phone: { type: String }
+           , email: { type: String }
+           , password: { type: String }
+}, { collection: 'usercollection' })
+
+userModel = mongoose.model('usercollection', userSchema)
+
+module.exports = {
+
+    getUserModel: function () {
+
+        if (mongoose.connection.readyState == 0) {
+            mongoose.connect(config.db);
+        }
+
+        return userModel;
+
+    }
+}
