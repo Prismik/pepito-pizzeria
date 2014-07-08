91acb68f45f19711d970620055bd9ead6cbb5bba
Philippe Richard <philippe.richard93@gmail.com>
2014-07-07 21:32:44 -0400

    Fixed formating error

--- public/javascripts/validateform.js
+++ public/javascripts/validateform.js
@@ -53,10 +53,12 @@ function validateEmail(inputId, errormessage){
 
 function addAddressInput() {
     var lastdiv = $(".addressDiv").last();
-    var num = parseInt(lastdiv.children("input").attr("data-num")) + 1
+    var num = parseInt(lastdiv.children("input").attr("data-num")) + 1;
 
     var newdiv = lastdiv.clone();
-    newdiv.children("label").html(" ")
+    newdiv.children("label").html(" ");
+    newdiv.children("input").first().attr("data-num",num);
+    newdiv.children("input").last().attr("data-num",num);
     newdiv.children("input").first().val("");
     newdiv.children("input").last().val("");
     newdiv.children("button").attr("onclick","delAddressInput("+num+")").html("Delete");
@@ -67,4 +69,5 @@ function addAddressInput() {
 function delAddressInput(num) {
     var div = $('input[data-num="'+num+'"]').parent();
     div.remove();
+     $(this).parent().remove();
 }
